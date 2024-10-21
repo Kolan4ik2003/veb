d513236a8bc2ed066d1c1d6934a2264e62d5eb89
Kolan4ik2003 <zarembiczkiy@mail.ru>
2024-10-21 21:58:49 +0300

    2110/1

--- veb-top/src/components/Sale/Catalog.vue
+++ veb-top/src/components/Sale/Catalog.vue
@@ -6,6 +6,6 @@ defineProps({
 
 <template>
         <h3>{{ catalog.nameverx }}</h3>   
-        <img src="https://avatars.mds.yandex.net/i?id=c50912e65ffec3d061bb9b5cdbe38a5b_l-9236689-images-thumbs&n=13" alt="Букет Роз" width="300">
+        <img :src="catalog.image" :alt="catalog.nameverx" width="300">
         <p><a><router-link :to="{name: 'content-info' , params: {id:catalog.id}} ">{{ catalog.name }} {{ catalog.price }}</router-link></a></p>
 </template>
--- veb-top/src/views/HomeView.vue
+++ veb-top/src/views/HomeView.vue
@@ -1,45 +1,52 @@
 <script setup>
 import Catalog from '@/components/Sale/Catalog.vue';
+import {ref} from 'vue'
 const catalogs = [
     {
         id : 1,
+        image : 'https://avatars.mds.yandex.net/i?id=c50912e65ffec3d061bb9b5cdbe38a5b_l-9236689-images-thumbs&n=13',
         nameverx : "Букет роз",
         name : "Прекрасный букет из свежих роз:",
         price : 2000
     },
     {
         id : 2,
+        image : 'https://avatars.mds.yandex.net/i?id=5d77f84f632f003f9f6309d31a027985_l-4274684-images-thumbs&n=13',
         nameverx : "Тюльпаны",
         name : "Яркие весенние тюльпаны:",
         price : 100 
     },
     {
         id : 3,
+        image : 'https://i.pinimg.com/736x/9e/4a/8a/9e4a8a4a31e4a54c053810f4dd039b6f.jpg',
         nameverx : "Пионы",
         name : "Незабываемые пионы:",
         price : 450 
     },
     {
         id : 4,
+        image : 'https://s.leroymerlin.kz/upload/catalogue/product_images/f/7/original/18793109.jpg',
         nameverx : "Лилии",
         name : "Самые ароматные лилии:",
         price : 350 
     },
     {
         id : 5,
+        image : 'https://avatars.mds.yandex.net/i?id=976a12dfd50eaf238457a52d16bc6428_l-5258284-images-thumbs&n=13',
         nameverx : "Гортензии",
         name : "Великолепные гортензии:",
         price : 500 
     },
     {
         id : 6,
+        image : 'https://avatars.mds.yandex.net/get-yabs_performance/13451990/hat20708ce1c32cd9b9cadf363e32f9c5cc/hugeXX',
         nameverx : "Хризантемы",
         name : "Чудесные белые хризантемы:",
         price : 1500 
     }
 ]
 
-import {ref} from 'vue'
+
 const searchResult = ref('')
 const isShowSearch = ref(false)
 const search = () => {
